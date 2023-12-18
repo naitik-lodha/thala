ee1b0496ea6581786dc9ddc64162224f5017bc85
Naitik Lodha <naitik.lodha139@nmims.edu.in>
2023-12-18 22:59:01 +0530

    Added stopped audio button

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 "use client";
 import { checkForThala } from "./utils/checkForThala";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Head from "next/head";
+
 export default function Home() {
   const inputRef = useRef();
+  const [audioPlayer, setAudioPlayer] = useState(null);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const ThalaAudio = "/audio/ThalaAudio.mp3";
     const result = checkForThala(inputRef.current.value);
+    
     if (result !== null) {
       const audio = new Audio(ThalaAudio);
-      audio
-        .play()
+      setAudioPlayer(audio); // Set the audio player in state to control it later
+      
+      audio.play()
         .then(() => {
           alert(result);
         })
@@ -24,8 +29,19 @@ export default function Home() {
     }
   };
 
+  const stopAudio = () => {
+    if (audioPlayer) {
+      audioPlayer.pause(); 
+      audioPlayer.currentTime = 0; 
+      setAudioPlayer(false)
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col bg-yellow-500 text-black items-center justify-center p-8">
+      <Head>
+        <title>Thala Checker</title>
+      </Head>
       <div className="mb-6">
         <img
           src="https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS_Q-fRFfDWAEfOcz_R0vktnglTXYO4EKn9z8bBlX6mgl4cobbtQ8K4KZFq46SSM4ArMq51ma5AZGxc62Y"
@@ -46,6 +62,15 @@ export default function Home() {
         >
           Check for Thala!
         </button>
+        {audioPlayer && (
+          <button
+            type="button"
+            onClick={stopAudio}
+            className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-6 rounded-md transition duration-300"
+          >
+            Stop Audio
+          </button>
+        )}
       </form>
     </main>
   );
